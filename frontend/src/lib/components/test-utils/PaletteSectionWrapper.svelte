fc854acb833986ff15789ee5e45e09e7356849c2
Bojan Rajkovic <brajkovic@coderinserepeat.com>
2026-04-27 07:04:36 -0400

    feat(frontend): add PaletteSection pure leaf for Command.Group wrapping

---
+++ frontend/src/lib/components/test-utils/PaletteSectionWrapper.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  import PaletteSection from '../PaletteSection.svelte'
+  import * as Command from '../ui/command'
+
+  let { heading, content }: { heading: string; content: string } = $props()
+</script>
+
+<Command.Root>
+  <Command.List>
+    <PaletteSection {heading}>
+      {#if content}
+        <div>{content}</div>
+      {/if}
+    </PaletteSection>
+  </Command.List>
+</Command.Root>
